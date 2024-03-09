479c6170ade45a55f64f9f5af967e6e491059ce4
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-09 09:47:46 +0530

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import Icon from './Icon.jsx'
 function Home() {
   return (
-    <div className=''>
-
+    <div className='flex justify-center items-center h-screen'>
+      <Icon/>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
