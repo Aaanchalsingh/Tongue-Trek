efc18fb61cbf25557c8d0d9757c227bbd3476422
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-09 09:01:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
 function App() {
+  const [apiResponse, setAPIResponse]=useState("");
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
+  const callAPI=() => {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => setAPIResponse(res))
+      .catch(error => console.log(error)); // Handle any errors
+  };
+  const [username, setUsername]=useState('');
+  const [password, setPassword]=useState('');
+  const [loginStatus, setLoginStatus]=useState('');
+
+  const handleLogin=() => {
+    if (username==='admin'&&password==='admin') {
+      setLoginStatus('Login successful');
+    } else {
+      setLoginStatus('Invalid username or password');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <div className="login-container">
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Login</button>
+        <p className="App-intro">{apiResponse}</p>
+      </div>
+    </center>
   );
 }
 
 export default App;
+
+
