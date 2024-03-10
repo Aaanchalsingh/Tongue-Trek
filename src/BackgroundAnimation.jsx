e09798643598906b3c2b7c26c8e30828ee176076
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-10 20:28:42 +0530

    commit

--- src/BackgroundAnimation.jsx
+++ src/BackgroundAnimation.jsx
@@ -1,65 +1,99 @@
 import React, { useEffect } from "react";
+import $ from "jquery";
 import "jquery.transit";
 import { Link } from "react-router-dom";
-import $ from "jquery";
 
 function BackgroundAnimation() {
   useEffect(() => {
-    const initializr = () => {
-      const style = {
+    const background = {};
+
+    background.initializr = function () {
+      const $this = this;
+
+      // Options
+      $this.id = "background_css3";
+      $this.style = {
         bubbles_color: "#fff",
         stroke_width: 0,
         stroke_color: "black",
       };
-      const bubbles_number = 30;
-      const speed = [1500, 8000];
-      const shape = false;
+      $this.bubbles_number = 30;
+      $this.speed = [1500, 8000];
+      $this.max_bubbles_height = $this.height;
+      $this.shape = false;
+      console.log("hello");
 
-      const object = document.createElement("div");
-      object.id = "background_css3";
-      object.style.cssText =
-        "z-index: -1; margin: 0; padding: 0; overflow: hidden; position: absolute; bottom: 0;";
-      document.body.appendChild(object);
+      if ($("#" + $this.id).length > 0) {
+        $("#" + $this.id).remove();
+      }
+      $this.object = $(
+        "<div style='margin:0;padding:0; overflow:hidden;position:absolute;bottom:0' id='" +
+          $this.id +
+          "'> </div>"
+      ).appendTo("body");
 
-      for (let i = 0; i < bubbles_number; i++) {
-        generateBubbles(object, style, shape, speed);
+      $this.ww = $(window).width();
+      $this.wh = $(window).height();
+      $this.width = $this.object.width($this.ww);
+      $this.height = $this.object.height($this.wh);
+
+      $("body").prepend(
+        "<style>.shape_background {transform-origin:center; width:80px; height:80px; background: " +
+          $this.style.bubbles_color +
+          "; position: absolute}</style>"
+      );
+
+      for (let i = 0; i < $this.bubbles_number; i++) {
+        $this.generate_bubbles();
       }
     };
 
-    const generateBubbles = (object, style, shape, speed) => {
-      const base = document.createElement("div");
-      base.classList.add("shape_background");
-
+    background.generate_bubbles = function () {
+      const $this = this;
+      const base = $("<div class='shape_background'></div>");
+      const shape_type = $this.shape ? $this.shape : Math.floor($this.rn(1, 3));
       let bolla;
-      const shape_type = shape ? shape : Math.floor(rn(1, 3));
-      if (shape_type === 1) {
-        bolla = base;
-      } else if (shape_type === 2) {
-        bolla = base;
+      if (shape_type == 1) {
+        bolla = base.css({ borderRadius: "50%" });
+      } else if (shape_type == 2) {
+        bolla = base.css({
+          width: 0,
+          height: 0,
+          "border-style": "solid",
+          "border-width": "0 40px 69.3px 40px",
+          "border-color":
+            "transparent transparent " +
+            $this.style.bubbles_color +
+            " transparent",
+          background: "transparent",
+        });
       } else {
         bolla = base;
       }
-
-      const rn_size = rn(0.8, 1.2);
-      bolla.style.cssText = `
-        transform: scale(${rn_size}) rotate(${rn(-360, 360)}deg);
-        top: 100vh;
-        left: ${rn(-60, window.innerWidth + 60)}px;
-      `;
-
-      object.appendChild(bolla);
-      $(bolla).transit(
+      const rn_size = $this.rn(0.8, 1.2);
+      bolla.css({
+        transform:
+          "scale(" + rn_size + ") rotate(" + $this.rn(-360, 360) + "deg)",
+        top: $this.wh + 100,
+        left: $this.rn(-60, $this.ww + 60),
+      });
+      bolla.appendTo($this.object);
+      bolla.transit(
         {
-          top: rn(window.innerHeight / 2, window.innerHeight / 2 - 60),
-          transform: `scale(${rn_size}) rotate(${rn(-360, 360)}deg)`,
+          top: $this.rn($this.wh / 2, $this.wh / 2 - 60),
+          transform:
+            "scale(" + rn_size + ") rotate(" + $this.rn(-360, 360) + "deg)",
           opacity: 0,
         },
-        rn(speed[0], speed[1]),
-        () => $(bolla).remove()
+        $this.rn($this.speed[0], $this.speed[1]),
+        function () {
+          $(this).remove();
+          $this.generate_bubbles();
+        }
       );
     };
 
-    const rn = (from, to, arr) => {
+    background.rn = function (from, to, arr) {
       if (arr) {
         return Math.random() * (to - from + 1) + from;
       } else {
@@ -67,61 +101,37 @@ function BackgroundAnimation() {
       }
     };
 
-    initializr();
+    background.initializr();
 
     return () => {};
   }, []);
 
   return (
-    <div className="App" style={{ background: "#007bff", height: "100%" }}>
-      hellllllllllllllo
-      <header className="App-header">
-        <h1
-          style={{
-            height: "auto",
-            display: "table",
-            width: "80%",
-            textAlign: "center",
-            overflow: "auto",
-            margin: "auto",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            color: "white",
-            fontFamily: "'Raleway', sans-serif",
-            fontWeight: 300,
-            fontSize: "3em",
-          }}
-        >
-          Voice-to-Text Recognition
-        </h1>
-        <h2
-          style={{
-            position: "fixed",
-            textAlign: "center",
-            top: "10px",
-            right: "20px",
-            color: "white",
-            fontSize: "20px",
-            fontFamily: "Roboto",
-            fontWeight: "normal",
-            zIndex: 999,
-          }}
-        >
-          <span style={{ fontSize: "15px", fontWeight: "lighter" }}>
-            Subtitle
-          </span>
-          <Link
-            to="#"
-            style={{ color: "white", fontSize: "15px", padding: "0 30px" }}
-          >
-            Link
-          </Link>
-        </h2>
-      </header>
+    <div className="App" style={{ background: "#007bff" }}>
+      <h1>Record Voice Notes</h1>
+      <div>
+        <div className="noteContainer">
+          <h2>Record Note Here</h2>
+          {isRecording ? <span>Recording... </span> : <span>Stopped </span>}
+          <button className="button" onClick={storeNote} disabled={!note}>
+            Save
+          </button>
+          <button onClick={() => setisRecording((prevState) => !prevState)}>
+            Start/Stop
+          </button>
+          <p>{note}</p>
+        </div>
+        <div className="noteContainer">
+          <h2>Notes Store</h2>
+          **
+          {notesStore.map((note) => (
+            <p key={note}>{note}</p>
+          ))}
+          **
+        </div>
+      </div>
     </div>
   );
 }
+
 export default BackgroundAnimation;
