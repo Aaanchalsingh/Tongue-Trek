a0cb2efa004dbb8bf7b1226a580e322c93137699
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-10 13:02:54 +0530

    commit

--- src/Pge2.jsx
+++ src/Pge2.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SpeechToText from "./SpeechToText";
+import BackgroundAnimation from "./BackgroundAnimation";
 function Pge2() {
   return (
     <div className="App">
     <header className="App-header">
       <h1>Voice-to-Text Recognition</h1>
+      <BackgroundAnimation/>
       <SpeechToText />
     </header>
   </div>
--- src/SpeechToText.jsx
+++ src/SpeechToText.jsx
@@ -1,33 +1,77 @@
-import React, { useState } from "react";
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from "react-speech-recognition";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-const SpeechToText = () => {
-  const [transcript, setTranscript] = useState("");
-  const { finalTranscript, resetTranscript } = useSpeechRecognition();
+function App() {
+  const [isRecording, setisRecording]=useState(false);
+  const [note, setNote]=useState(null);
+  const [notesStore, setnotesStore]=useState([]);
+  const SpeechRecognition=
+    window.SpeechRecognition||window.webkitSpeechRecognition;
+  const microphone=new SpeechRecognition();
 
-  const handleListen = () => {
-    SpeechRecognition.startListening({ continuous: true });
+  microphone.continuous=true;
+  microphone.interimResults=true;
+  microphone.lang="en-US";
+  const storeNote=() => {
+    setnotesStore([...notesStore, note]);
+    setNote("");
+  };
+  const startRecordController=() => {
+    if (isRecording) {
+      microphone.start();
+      microphone.onend=() => {
+        console.log("continue..");
+        microphone.start();
+      };
+    } else {
+      microphone.stop();
+      microphone.onend=() => {
+        console.log("Stopped microphone on Click");
+      };
+    }
+    microphone.onstart=() => {
+      console.log("microphones on");
+    };
+
+    microphone.onresult=(event) => {
+      const recordingResult=Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      console.log(recordingResult);
+      setNote(recordingResult);
+      microphone.onerror=(event) => {
+        console.log(event.error);
+      };
+    };
   };
 
-  const stopListening = () => {
-    SpeechRecognition.stopListening();
-    setTranscript(finalTranscript);
-    resetTranscript();
-  };
-
+  useEffect(() => {
+    startRecordController();
+     // eslint-disable-next-line
+  }, [isRecording]);
   return (
-    <div className="box">
-      <button className="btn text-white" onClick={handleListen}>
-        Start Listening
-      </button>
-      <button className="glow-on-hover" onClick={stopListening}>
-        Stop Listening
-      </button>
-      <p>{transcript}</p>
+    <div className="sppech">
+      <h1>Record Voice Notes</h1>
+      <div>
+        <div className="noteContainer">
+          <h2>Record Note Here</h2>
+          {isRecording? <span>Recording... </span>:<span>Stopped </span>}
+          <button className="button" onClick={storeNote} disabled={!note}>
+            Save
+          </button>
+          <button onClick={() => setisRecording((prevState) => !prevState)}>
+            Start/Stop
+          </button>
+          <p>{note}</p>
+        </div>
+        <div className="noteContainer">
+          <h2>Notes Store</h2>
+          **{notesStore.map((note) => (
+            <p key={note}>{note}</p>
+          ))}**
+        </div>
+      </div>
     </div>
-  );
-};
-
-export default SpeechToText;
+  );}
+export default App;
